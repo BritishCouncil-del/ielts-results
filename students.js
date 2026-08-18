e7abca0cc2f94f26b0c53f07ddb47e21122edcfb
BritishCouncil-del <britishcouncil719@gmail.com>
2026-08-18 23:46:19 +0300

    Add student data for BUTI SAEED SALEM ALMAHDHI

---
+++ students.js
@@ -0,0 +1,17 @@
+const students = {
+  "26AE509284ALSLB113A": {
+    name: "BUTI SAEED SALEM ALMAHDHI",
+    candidateNumber: "509284",
+    dob: "18/02/2009",
+    nationality: "الإمارات العربية المتحدة",
+    listening: 6.0,
+    reading: 5.5,
+    writing: 5.5,
+    speaking: 6.5,
+    overall: 6.0,
+    cefr: "B2",
+    examDate: "11/08/2026",
+    trf: "26AE509284ALSLB113A",
+    image: "images/student2.jpg"
+  }
+};
