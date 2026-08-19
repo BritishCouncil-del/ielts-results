4b35b9713076388b1dd86c668124822e53839ad1
BritishCouncil-del <britishcouncil719@gmail.com>
2026-08-19 10:34:50 +0300

    Modify student data structure and types
    Updated student details with new centre information and changed score types to strings.

--- students.js
+++ students.js
@@ -1,19 +1,24 @@
 var students = {
   "26AE509284ALSB113A": {
-    name: "BUTI SAEED SALEM ALMAHDHI",
-    familyName: "ALSHAMSI",
+    centreNumber: "AE113",
+    centreName: "BRITISH COUNCIL",
     candidateNumber: "509284",
+    candidateID: "784-2009-7640391-3",
+    familyName: "ALSHAMSI",
+    name: "BUTI SAEED SALEM ALMAHDHI",
     dob: "18/02/2009",
-    nationality: "UNITED ARAB EMIRATES",
-    firstLanguage: "ARABIC",
+    sex: "M",
+    repeat: "N",
+    prevDate: "-",
+    prevCentre: "-",
     examDate: "11/08/2026",
     trf: "26AE509284ALSB113A",
-    listening: 6.0,
-    reading: 5.5,
-    writing: 5.5,
-    speaking: 6.5,
-    overall: 6.0,
+    listening: "6.0",
+    reading: "5.5",
+    writing: "5.5",
+    speaking: "6.5",
+    overall: "6.0",
     ceFR: "B2",
-    image: "buti.jpg"
+    image: "buti.jpg" // اسم الصورة اللي رفعتها في مجلد images
   }
 };
