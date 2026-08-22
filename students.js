2d1df89979c3cbdd776fe3bba69b24cd00aa6588
BritishCouncil-del <britishcouncil719@gmail.com>
2026-08-23 01:16:55 +0300

    Add new student and update exam date

--- students.js
+++ students.js
@@ -12,7 +12,7 @@ var students = {
     repeat: "N",
     prevDate: "-",
     prevCentre: "-",
-    examDate: "11/08/2026",
+    examDate: "11/08/2026", 
     trf: "26AE507413ALSM113A",
     listening: "6.0",
     reading: "5.5",
@@ -45,5 +45,31 @@ var students = {
     overall: "6.0",
     ceFR: "B2",
     image: "buti.jpg"
+  }
+};
+var students = {
+  "26AE509284ALSB113A": {
+    candidate: "509284",
+    name: "BUTI SAEED SALEM ALMAHDHI",
+    dob: "18/02/2009",
+    examDate: "11/08/2026",
+    listening: "6.0",
+    reading: "5.5",
+    writing: "5.5",
+    speaking: "6.5",
+    overall: "6.0",
+    image: "buti.jpg"
+  },
+  "26AE509426ALBM113A": {   // الطالب الجديد
+    candidate: "509426",
+    name: "MOHAMMAD ALI KHAMIS HAMAD ALBEDWAWI",
+    dob: "19/07/2008",
+    examDate: "22/08/2026",
+    listening: "5.0",
+    reading: "5.5",
+    writing: "5.5",
+    speaking: "6.0",
+    overall: "5.5",
+    image: "albedwawi.png"
   }
 };
