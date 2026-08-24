ae15260fe74184c34c960b965d665bce31bc6db4
BritishCouncil-del <britishcouncil719@gmail.com>
2026-08-24 23:32:12 +0300

    Modify student centre name and add comments
    Updated the centre name to uppercase and added comments for adding new students.

--- students.js
+++ students.js
@@ -1,8 +1,11 @@
 var students = {
+  // الطالب الأول
   "2GAE509426ALBM113A": {
     candidate: "509426",
+    candidateId: "784-2008-9639313-7",
     name: "MOHAMMAD ALI KHAMIS HAMAD ALBEDWAWI",
     dob: "19/07/2008",
+    sex: "M",
     examDate: "22/08/2026",
     overall: "5.5",
     listening: "5.0",
@@ -10,8 +13,29 @@ var students = {
     writing: "5.5",
     speaking: "6.0",
     centre: "AE113",
-    centreName: "British Council",
+    centreName: "BRITISH COUNCIL",
     trfNumber: "2GAE509426ALBM113A",
     image: "albedwawi.png"
   }
+
+  // هنا تقدر تضيف أي طالب جديد بنفس الشكل 👇
+  /*
+  "TRF_NUMBER_الجديد": {
+    candidate: "رقم المرشح",
+    candidateId: "ID الطالب",
+    name: "اسم الطالب بالكامل",
+    dob: "تاريخ الميلاد",
+    sex: "M/F",
+    examDate: "تاريخ الامتحان",
+    overall: "الدرجة الكلية",
+    listening: "درجة الاستماع",
+    reading: "درجة القراءة",
+    writing: "درجة الكتابة",
+    speaking: "درجة المحادثة",
+    centre: "رقم المركز",
+    centreName: "اسم المركز",
+    trfNumber: "TRF_NUMBER_الجديد",
+    image: "اسم ملف الصورة.png"
+  }
+  */
 };
