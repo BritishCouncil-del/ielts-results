cbfa1659618e28d29329105dd89d24e808ac6943
BritishCouncil-del <britishcouncil719@gmail.com>
2026-08-24 19:22:30 +0300

    Update students.js

--- students.js
+++ students.js
@@ -3,36 +3,12 @@ var students = {
     candidate: "509426",
     name: "MOHAMMAD ALI KHAMIS HAMAD ALBEDWAWI",
     dob: "19/07/2008",
-    examDate: "11/08/2026",
+    examDate: "22/08/2026",
     overall: "5.5",
     listening: "5.0",
     reading: "5.5",
     writing: "5.5",
     speaking: "6.0",
     image: "albedwawi.png"
-  },
-  "26AE509284ALSB113A": {
-    candidate: "509284",
-    name: "BUTI SAEED SALEM ALMAHDHI ALSHAMSI",
-    dob: "18/02/2009",
-    examDate: "11/08/2026",
-    overall: "6.0",
-    listening: "6.0",
-    reading: "5.5",
-    writing: "5.5",
-    speaking: "6.5",
-    image: "baut.png"
-  },
-  "26AE507413ALSM113A": {
-    candidate: "507413",
-    name: "MOHAMED AHMED ABDULLA ALRUDAMI ALSHEHHI",
-    dob: "06/06/2002",
-    examDate: "11/08/2026",
-    overall: "6.0",
-    listening: "6.0",
-    reading: "5.5",
-    writing: "5.5",
-    speaking: "6.5",
-    image: "Mohamed.jpg"
   }
 };
