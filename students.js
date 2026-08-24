e225e840f3b2ae58e9ba5a84ef0605c6b661801a
BritishCouncil-del <britishcouncil719@gmail.com>
2026-08-24 16:57:58 +0300

    Refactor student data structure
    Removed unused student data and updated existing entries.

--- students.js
+++ students.js
@@ -1,55 +1,38 @@
-// ملف بيانات الطلاب
 var students = {
-  // ✅ مثال طالب 1
-  "26AE509284ALSB113A": {
-    candidate: "509284",
-    id: "784-2002-9850508-0",   // Candidate ID
-    family: "ALMAHDHI",         // Family Name
-    name: "BUTI SAEED SALEM",   // First Name(s)
-    dob: "18/02/2009",
-    sex: "M",
-    examDate: "11/08/2026",
-    listening: "6.0",
-    reading: "5.5",
-    writing: "5.5",
-    speaking: "6.5",
-    overall: "6.0",
-    image: "buti.jpg"
-  },
-
-  // ✅ مثال طالب 2
-  "26AE509426ALBM113A": {
+  "2GAE509426ALBM113A": {
     candidate: "509426",
-    id: "784-2008-9850508-1",
-    family: "ALBEDWAWI",
-    name: "MOHAMMAD ALI KHAMIS HAMAD",
+    name: "MOHAMMAD ALI KHAMIS HAMAD ALBEDWAWI",
     dob: "19/07/2008",
-    sex: "M",
-    examDate: "22/08/2026",
+    examDate: "11/08/2026",
+    overall: "5.5",
     listening: "5.0",
     reading: "5.5",
     writing: "5.5",
     speaking: "6.0",
-    overall: "5.5",
     image: "albedwawi.png"
   },
-
-  // 🆕 انسخ البلوك ده لأي طالب جديد
-  /*
-  "SERIAL_NUMBER_HERE": {
-    candidate: "CANDIDATE_NUMBER_HERE",
-    id: "CANDIDATE_ID_HERE",
-    family: "FAMILY_NAME_HERE",
-    name: "FIRST_NAME_HERE",
-    dob: "DD/MM/YYYY",
-    sex: "M/F",
-    examDate: "DD/MM/YYYY",
-    listening: "X.X",
-    reading: "X.X",
-    writing: "X.X",
-    speaking: "X.X",
-    overall: "X.X",
-    image: "studentPhoto.png"
+  "26AE509284ALSB113A": {
+    candidate: "509284",
+    name: "BUTI SAEED SALEM ALMAHDHI ALSHAMSI",
+    dob: "18/02/2009",
+    examDate: "11/08/2026",
+    overall: "6.0",
+    listening: "6.0",
+    reading: "5.5",
+    writing: "5.5",
+    speaking: "6.5",
+    image: "baut.png"
+  },
+  "26AE507413ALSM113A": {
+    candidate: "507413",
+    name: "MOHAMED AHMED ABDULLA ALRUDAMI ALSHEHHI",
+    dob: "06/06/2002",
+    examDate: "11/08/2026",
+    overall: "6.0",
+    listening: "6.0",
+    reading: "5.5",
+    writing: "5.5",
+    speaking: "6.5",
+    image: "Mohamed.jpg"
   }
-  */
 };
