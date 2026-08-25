9a2726ac308528bda76a1190182a394527bf3d77
BritishCouncil-del <britishcouncil719@gmail.com>
2026-08-25 20:02:31 +0300

    Add new student record for Mohammad Ali

--- students.js
+++ students.js
@@ -1,19 +1,19 @@
 var students = {
-  "26UK654321ALBM002B": {
-    candidate: "654321",
-    candidateId: "UK-1999-1234567-9",
-    name: "FATIMA ALI",
-    dob: "02/11/1999",
-    sex: "F",
-    examDate: "05/08/2026",
-    overall: "8.0",
-    listening: "8.5",
-    reading: "8.0",
-    writing: "7.5",
-    speaking: "8.0",
-    centre: "UK002",
-    centreName: "CAMBRIDGE ENGLISH",
-    trfNumber: "26UK654321ALBM002B",
-    image: "fatima.png"
+  "2GAE509426ALBM113A": {
+    candidate: "509426",
+    candidateId: "784-2008-9639313-7",
+    name: "MOHAMMAD ALI KHAMIS HAMAD ALBEDWAWI",
+    dob: "19/07/2008",
+    sex: "M",
+    examDate: "22/08/2026",
+    overall: "5.5",
+    listening: "5.0",
+    reading: "5.5",
+    writing: "5.5",
+    speaking: "6.0",
+    centre: "AE113",
+    centreName: "BRITISH COUNCIL",
+    trfNumber: "2GAE509426ALBM113A",
+    image: "ALBEDWAWI.png"
   }
 };
