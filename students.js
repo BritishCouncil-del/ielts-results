69a2422d29dc37b9b78d44328677f318400e6f91
BritishCouncil-del <britishcouncil719@gmail.com>
2026-08-25 13:43:50 +0300

    Update students.js

--- students.js
+++ students.js
@@ -1,5 +1,4 @@
 var students = {
-  // الطالب الأول
   "2GAE509426ALBM113A": {
     candidate: "509426",
     candidateId: "784-2008-9639313-7",
@@ -16,24 +15,5 @@ var students = {
     centreName: "BRITISH COUNCIL",
     trfNumber: "2GAE509426ALBM113A",
     image: "albedwawawi.png"
-  },
-
-  // الطالب الثاني (مثال تجريبي)
-  "2GAE509999ALBM113B": {
-    candidate: "509999",
-    candidateId: "784-2005-1234567-8",
-    name: "ALI HASSAN MOHAMED",
-    dob: "01/01/2005",
-    sex: "M",
-    examDate: "25/08/2026",
-    overall: "6.0",
-    listening: "6.5",
-    reading: "6.0",
-    writing: "5.5",
-    speaking: "6.0",
-    centre: "AE113",
-    centreName: "BRITISH COUNCIL",
-    trfNumber: "2GAE509999ALBM113B",
-    image: "alihassan.png"
   }
 };
