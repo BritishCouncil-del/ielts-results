45aa0ccad58ab7ab4168dfdc8390cbbea7361094
BritishCouncil-del <britishcouncil719@gmail.com>
2026-08-26 22:40:37 +0300

    Refactor students data structure and add new student

--- students.js
+++ students.js
@@ -1,19 +1,39 @@
-var students = {
-  "26AE509426ALBM113A": {
+const students = {
+  // 👤 طالب قديم (مثال موجود عندك)
+  "2GAE509426ALBM113A": {
     candidate: "509426",
-    candidateId: "784-2008-9639313-7",
+    candidateId: "C001",
     name: "MOHAMMAD ALI KHAMIS HAMAD ALBEDWAWI",
-    dob: "19/07/2008",
+    dob: "2008-07-19",
     sex: "M",
-    examDate: "22/08/2026",
+    examDate: "2026-08-22",
     overall: "5.5",
     listening: "5.0",
     reading: "5.5",
     writing: "5.5",
     speaking: "6.0",
+    trfNumber: "2GAE509426ALBM113A",
     centre: "AE113",
-    centreName: "BRITISH COUNCIL",
-    trfNumber: "26AE509426ALBM113A",
-    image: "albedwawi.png"   // ← نفس اسم الصورة بالضبط
+    centreName: "British Council",
+    image: "albedwawi.png"
+  },
+
+  // 👤 الطالب الجديد من تقرير الـ IELTS
+  "26EG009471ABDA001A": {
+    candidate: "009471",              // رقم المرشح
+    candidateId: "A04468374",         // رقم الهوية
+    name: "AHMED ABDELAATY ABDELHALIM",
+    dob: "1995-01-01",
+    sex: "M",
+    examDate: "2026-08-22",
+    overall: "7.0",
+    listening: "7.5",
+    reading: "7.0",
+    writing: "6.0",
+    speaking: "6.5",
+    trfNumber: "26EG009471ABDA001A",  // رقم السيريال/TRF
+    centre: "EG001",
+    centreName: "British Council",
+    image: "abdelaaty.png"            // ضع صورة الطالب في مجلد images بنفس الاسم
   }
 };
