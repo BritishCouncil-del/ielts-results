2000cb30abdba058c0f0ba39e597fc218afe7cd6
BritishCouncil-del <britishcouncil719@gmail.com>
2026-09-01 17:25:55 +0300

    Add new student entries and update existing ones

--- students.js
+++ students.js
@@ -1,8 +1,10 @@
 const students = {
-  // الطالب القديم (السيريال يبدأ بـ 26)
+  // الطالب القديم
   "26AE509426ALBM113A": {
     candidate: "509426",
     candidateId: "C001",
+    familyName: "ALBEDWAWI",
+    firstName: "MOHAMMAD ALI KHAMIS HAMAD",
     name: "MOHAMMAD ALI KHAMIS HAMAD ALBEDWAWI",
     dob: "2008-07-19",
     sex: "M",
@@ -18,10 +20,12 @@ const students = {
     image: "albedwawi.png"
   },
 
-  // الطالب الجديد
+  // الطالب أحمد
   "26EG009471ABDA001A": {
     candidate: "009471",
     candidateId: "A04468374",
+    familyName: "ABDELHALIM",
+    firstName: "AHMED ABDELAATY",
     name: "AHMED ABDELAATY ABDELHALIM",
     dob: "1995-01-01",
     sex: "M",
@@ -35,5 +39,26 @@ const students = {
     centre: "EG001",
     centreName: "British Council",
     image: "abdelaaty.png"
+  },
+
+  // الطالب الجديد من TRF
+  "26AE509284ALSB113A": {
+    candidate: "509284",
+    candidateId: "784-2009-7640391-3",
+    familyName: "ALSHAMSI",
+    firstName: "BUTI SAEED SALEM ALMAHDHI",
+    name: "BUTI SAEED SALEM ALMAHDHI ALSHAMSI",
+    dob: "2009-02-18",
+    sex: "M",
+    examDate: "2026-08-11",
+    overall: "6.0",
+    listening: "6.0",
+    reading: "5.5",
+    writing: "5.5",
+    speaking: "6.5",
+    trfNumber: "26AE509284ALSB113A",
+    centre: "AE113",
+    centreName: "British Council",
+    image: "alshamsi.png"
   }
 };
