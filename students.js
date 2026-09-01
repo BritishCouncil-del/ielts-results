a83884a61199b884e03add8e292913df3963634e
BritishCouncil-del <britishcouncil719@gmail.com>
2026-09-01 18:33:29 +0300

    Clean up students.js by removing comments and entries
    Removed comments and unused student entries from the students object.

--- students.js
+++ students.js
@@ -1,5 +1,4 @@
 const students = {
-  // الطالب القديم
   "26AE509426ALBM113A": {
     candidate: "509426",
     candidateId: "C001",
@@ -20,7 +19,6 @@ const students = {
     image: "albedwawi.png"
   },
 
-  // الطالب أحمد
   "26EG009471ABDA001A": {
     candidate: "009471",
     candidateId: "A04468374",
@@ -35,30 +33,4 @@ const students = {
     reading: "7.0",
     writing: "6.0",
     speaking: "6.5",
-    trfNumber: "26EG009471ABDA001A",
-    centre: "EG001",
-    centreName: "British Council",
-    image: "abdelaaty.png"
-  },
-
-  // الطالب الجديد من TRF
-  "26AE509284ALSB113A": {
-    candidate: "509284",
-    candidateId: "784-2009-7640391-3",
-    familyName: "ALSHAMSI",
-    firstName: "BUTI SAEED SALEM ALMAHDHI",
-    name: "BUTI SAEED SALEM ALMAHDHI ALSHAMSI",
-    dob: "2009-02-18",
-    sex: "M",
-    examDate: "2026-08-11",
-    overall: "6.0",
-    listening: "6.0",
-    reading: "5.5",
-    writing: "5.5",
-    speaking: "6.5",
-    trfNumber: "26AE509284ALSB113A",
-    centre: "AE113",
-    centreName: "British Council",
-    image: "baut.png"   // ← مطابق لاسم الملف عندك بالضبط
-  }
-};
+    trfNumber:
