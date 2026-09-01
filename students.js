e1ad1ed5f969570a8508cd998a5429031dab4189
BritishCouncil-del <britishcouncil719@gmail.com>
2026-09-01 20:49:50 +0300

    Refactor students data structure from object to array

--- students.js
+++ students.js
@@ -1,64 +1,50 @@
-const students = {
-  // الطالب القديم
-  "26AE509426ALBM113A": {
-    candidate: "509426",
-    candidateId: "C001",
-    familyName: "ALBEDWAWI",
-    firstName: "MOHAMMAD ALI KHAMIS HAMAD",
-    name: "MOHAMMAD ALI KHAMIS HAMAD ALBEDWAWI",
-    dob: "2008-07-19",
-    sex: "M",
-    examDate: "2026-08-22",
-    overall: "5.5",
-    listening: "5.0",
-    reading: "5.5",
-    writing: "5.5",
-    speaking: "6.0",
-    trfNumber: "26AE509426ALBM113A",
-    centre: "AE113",
+const students = [
+  {
+    candidateNumber: "507413",
+    trfNumber: "26AE507413ALSM113A",
+    name: "Mohamed Ahmed Abdallah Al-Rudamy",
+    dob: "06/06/2002",
+    gender: "M",
+    centreNumber: "AE113",
     centreName: "British Council",
-    image: "albedwawi.png"
-  },
-
-  // الطالب أحمد
-  "26EG009471ABDA001A": {
-    candidate: "009471",
-    candidateId: "A04468374",
-    familyName: "ABDELHALIM",
-    firstName: "AHMED ABDELAATY",
-    name: "AHMED ABDELAATY ABDELHALIM",
-    dob: "1995-01-01",
-    sex: "M",
-    examDate: "2026-08-22",
-    overall: "7.0",
-    listening: "7.5",
-    reading: "7.0",
-    writing: "6.0",
-    speaking: "6.5",
-    trfNumber: "26EG009471ABDA001A",
-    centre: "EG001",
-    centreName: "British Council",
-    image: "abdelaaty.png"
-  },
-
-  // الطالب الجديد من TRF
-  "26AE509284ALSB113A": {
-    candidate: "509284",
-    candidateId: "784-2009-7640391-3",
-    familyName: "ALSHAMSI",
-    firstName: "BUTI SAEED SALEM ALMAHDHI",
-    name: "BUTI SAEED SALEM ALMAHDHI ALSHAMSI",
-    dob: "2009-02-18",
-    sex: "M",
-    examDate: "2026-08-11",
-    overall: "6.0",
     listening: "6.0",
     reading: "5.5",
     writing: "5.5",
     speaking: "6.5",
-    trfNumber: "26AE509284ALSB113A",
-    centre: "AE113",
+    overall: "6.0",
+    testDate: "11/08/2026",
+    photo: "images/student1.jpg"
+  },
+  {
+    candidateNumber: "009471",
+    trfNumber: "26EG009471ABDA001A",
+    name: "Ahmed Abdelaaty Abdelhalim",
+    dob: "01/01/1995",
+    gender: "M",
+    centreNumber: "EG001",
     centreName: "British Council",
-    image: "baut.png"   // ← مطابق لاسم الملف عندك بالضبط
+    listening: "7.5",
+    reading: "7.0",
+    writing: "6.0",
+    speaking: "6.5",
+    overall: "7.0",
+    testDate: "22/08/2026",
+    photo: "images/abdelaaty.png"
+  },
+  {
+    candidateNumber: "509426",
+    trfNumber: "26AE509426ALBM113A",
+    name: "Mohammad Ali Khamis Hamad Albedwawi",
+    dob: "19/07/2008",
+    gender: "M",
+    centreNumber: "AE113",
+    centreName: "British Council",
+    listening: "5.0",
+    reading: "5.5",
+    writing: "5.5",
+    speaking: "6.0",
+    overall: "5.5",
+    testDate: "22/08/2026",
+    photo: "images/baut.png"
   }
-};
+];
