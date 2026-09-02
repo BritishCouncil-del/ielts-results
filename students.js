12ec342698dd2879b6e072febfc864957bacc163
BritishCouncil-del <britishcouncil719@gmail.com>
2026-09-02 14:00:23 +0300

    Refactor students data structure from array to object

--- students.js
+++ students.js
@@ -1,46 +1,50 @@
-const students = [
-  {
-    candidateNumber: "509284",
-    trfNumber: "26AE509284ALSB113A",
-    familyName: "ALSHAMSI",
-    firstNames: "BUTI SAEED SALEM ALMAHDHI",
+var students = {
+  "26AE509284ALSB113A": {
+    candidate: "509284",
+    name: "BUTI SAEED SALEM ALMAHDHI ALSHAMSI",
     dob: "18/02/2009",
-    sex: "M",
     examDate: "11/08/2026",
-    overallScore: "6.0",
-    photo: "images/baut.png"
+    overall: "6.0",
+    listening: "6.0",
+    reading: "5.5",
+    writing: "5.5",
+    speaking: "6.5",
+    image: "baut.png"
   },
-  {
-    candidateNumber: "509426",
-    trfNumber: "2GAE509426ALBM113A",
-    familyName: "ALBEDWAWI",
-    firstNames: "MOHAMMAD ALI KHAMIS HAMAD",
-    dob: "19/07/2008",
-    sex: "M",
-    examDate: "22/08/2026",
-    overallScore: "5.5",
-    photo: "images/albedwawi.png"
-  },
-  {
-    candidateNumber: "009471",
-    trfNumber: "26EG009471ABDA001A",
-    familyName: "ABDELAATY",
-    firstNames: "AHMED ABDELAATY ABDELHALIM",
+  "26EG009471ABDA001A": {
+    candidate: "009471",
+    name: "AHMED ABDELAATY ABDELHALIM",
     dob: "01/01/1995",
-    sex: "M",
     examDate: "22/08/2026",
-    overallScore: "7.0",
-    photo: "images/abdelaaty.png"
+    overall: "7.0",
+    listening: "7.5",
+    reading: "7.0",
+    writing: "6.0",
+    speaking: "6.5",
+    image: "abdelaaty.png"
   },
-  {
-    candidateNumber: "509287",
-    trfNumber: "26AE509287OMA116D",
-    familyName: "OMAR",
-    firstNames: "AHMED SAEED",
-    dob: "05/03/2008",
-    sex: "M",
-    examDate: "14/08/2026",
-    overallScore: "6.5",
-    photo: "images/student1.jpg"
+  "2GAE509426ALBM113A": {
+    candidate: "509426",
+    name: "MOHAMMAD ALI KHAMIS HAMAD ALBEDWAWI",
+    dob: "19/07/2008",
+    examDate: "22/08/2026",
+    overall: "5.5",
+    listening: "5.0",
+    reading: "5.5",
+    writing: "5.5",
+    speaking: "6.0",
+    image: "albedwawi.png"
+  },
+  "26AE507413ALSM113A": {
+    candidate: "507413",
+    name: "MOHAMED AHMED ABDULLA ALRUDAMI ALSHEHHI",
+    dob: "06/06/2002",
+    examDate: "11/08/2026",
+    overall: "6.0",
+    listening: "6.0",
+    reading: "5.5",
+    writing: "5.5",
+    speaking: "6.5",
+    image: "student1.jpg"
   }
-];
+};
